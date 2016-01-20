84792981a690ca73a90b2728cf58fefa19263282
User (Reber / Debian Jessie x64) <ctong@ska.ac.za>
2016-01-20 14:46:05 +0200

    Moved functions to .cpp file. Added decimalDegreesToDMS static function.

---
+++ CoordinatePosition.cpp
@@ -0,0 +1,28 @@
+//System includes
+
+//Library includes
+
+//Local includes
+#include "CoordinatePosition.h"
+
+
+AVN::cCoordinatePosition::cCoordinatePosition() :
+    m_dLatitude_deg(0.0),
+    m_dLongitude_deg(0.0),
+    m_fAltitude_mASL(0.0f),
+    m_strLabel("")
+{
+}
+
+bool AVN::cCoordinatePosition::compareLabel(cCoordinatePosition oPos1, cCoordinatePosition oPos2)
+{
+    //Invert the output of compare function for alphabetically ascending:
+    return !oPos1.m_strLabel.compare(oPos2.m_strLabel);
+}
+
+void AVN::cCoordinatePosition::decimalDegreesToDMS(double dDecimalDegrees, int32_t &i32Degrees, int32_t &i32Minutes, double &dSeconds)
+{
+    i32Degrees  = (int32_t)( dDecimalDegrees );
+    i32Minutes  = (int32_t)( (dDecimalDegrees - (double)i32Degrees) * 60.0);
+    dSeconds    = (int32_t)( (dDecimalDegrees - (double)i32Degrees - (double)i32Minutes / 60.0) * 60.0 * 60.0);
+}
--- CoordinatePosition.h
+++ CoordinatePosition.h
@@ -1,9 +1,13 @@
-
-
 #ifndef COORDINATEPOSITION_H
 #define COORDINATEPOSITION_H
 
+//System includes
 #include <string>
+#include <inttypes.h>
+
+//Library includes
+
+//Local includes
 
 namespace AVN
 {
@@ -16,20 +20,13 @@ struct cCoordinatePosition
     std::string m_strLabel;
 
     //Default constructor everything to zero.
-    cCoordinatePosition() :
-        m_dLatitude_deg(0.0),
-        m_dLongitude_deg(0.0),
-        m_fAltitude_mASL(0.0f),
-        m_strLabel("")
-    {
-    }
+    cCoordinatePosition();
 
     //Compared function on labels for sorting vectors of cCoordinatePostion by label
-    static bool compareLabel(cCoordinatePosition oPos1, cCoordinatePosition oPos2)
-    {
-        //Invert the output of compare function for alphabetically ascending:
-        return !oPos1.m_strLabel.compare(oPos2.m_strLabel);
-    }
+    static bool compareLabel(cCoordinatePosition oPos1, cCoordinatePosition oPos2);
+
+    //Convert from decimal degrees to DMS
+    static void decimalDegreesToDMS(double dDecimalDegrees, int32_t &i32Degrees, int32_t &i32Minutes, double &dSeconds);
 };
 
 }
